4ae84e51ad6f7df8c8f9b7cc14e5c6981a3f3935
AntonAndreevichMoroz <moroz-it@mail.ru>
2021-12-17 20:56:36 +0300

    Изменен скрипт savechangesn8n.sh. Data export теперь в многострочном формате

--- scripts/savechangesn8n.sh
+++ scripts/savechangesn8n.sh
@@ -1,2 +1,2 @@
-docker exec rmb-n8n n8n export:workflow --all --output=/home/node/data-export/workflow.json
-docker exec rmb-n8n n8n export:credentials --all --decrypted --output=/home/node/data-export/credentials.json
+docker exec rmb-n8n n8n export:workflow --all --pretty --output=/home/node/data-export/workflow.json
+docker exec rmb-n8n n8n export:credentials --all --decrypted --pretty --output=/home/node/data-export/credentials.json
